43cb1224c73817d25e0b1fb38ff3f2b61eca732b
Adeonir Kohl <adeonir@gmail.com>
2026-09-05 06:57:16 -0300

    chore: use vite's native tsconfig paths resolution in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,11 @@
 import react from '@astrojs/react'
 import { getViteConfig } from 'astro/config'
 import icons from 'unplugin-icons/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default getViteConfig(
   {
-    plugins: [tsconfigPaths(), icons({ compiler: 'jsx', jsx: 'react' })],
+    plugins: [icons({ compiler: 'jsx', jsx: 'react' })],
+    resolve: { tsconfigPaths: true },
   },
   {
     configFile: false,
